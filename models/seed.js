0cb2c2d43466f543dcfa03248cccba8f6dd1a957
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-03-01 09:48:15 -0500

    started to seed

---
+++ models/seed.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        name: "Brad Pitt",
+        dob: "12/18/1963",
+        bio: string,
+        notable films: ["Oceans Eleven" , "Fight Club", "Troy"],
+        img: "https://en.wikipedia.org/wiki/Brad_Pitt#/media/File:Brad_Pitt_2019_by_Glenn_Francis.jpg"
+    }
+]
