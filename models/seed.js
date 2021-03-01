70617b5d0832207f67f61248de220b8fe947dd77
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-03-01 16:15:18 -0500

    finished initial seed and basic index layout

--- models/seed.js
+++ models/seed.js
@@ -2,8 +2,126 @@ module.exports = [
     {
         name: "Brad Pitt",
         dob: "12/18/1963",
-        bio: string,
-        notable films: ["Oceans Eleven" , "Fight Club", "Troy"],
-        img: "https://en.wikipedia.org/wiki/Brad_Pitt#/media/File:Brad_Pitt_2019_by_Glenn_Francis.jpg"
+        bio: "William Bradley Pitt (born December 18, 1963) is an American actor and film producer. He has received multiple awards, including two Golden Globe Awards and an Academy Award for his acting, in addition to another Academy Award, another Golden Globe Award and a Primetime Emmy Award as producer under his production company, Plan B Entertainment.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY3MDUxMjkzMjI1OTIwMTcz/brad-pitt-attends-the-premiere-of-20th-century-foxs--square.jpg"
+    },
+    {
+        name: "Denzel Washington",
+        dob: "12/28/1954",
+        bio: "Denzel Washington first studied journalism at Fordham University but then discovered an interest in acting. He made his feature film debut in the comedy A Carbon Copy (1981) and was cast on the hit TV medical drama St. Elsewhere (1982-88). He went on to appear in several hit movies, including Philadelphia, Man on Fire, The Book of Eli, American Gangster and Flight, and won Oscars for his roles in Glory and Training Day. He received an Oscar nomination for his starring role in 2016's Fences, an adaptation of August Wilson's Tony and Pulitzer Prize-winning play, and for the 2017 film Roman J. Israel, Esq. ",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxODE3OTI4MjA1/denzel-washington-9524687-2-402.jpg"
+
+    },
+    {
+        name: "Hugh Jackman",
+        dob: "10/12/1968",
+        bio: "Born in Australia in 1968, actor Hugh Jackman first earned widespread attention by portraying the mutant Wolverine in the superhero blockbuster X-Men (2000). In addition to reprising the role though numerous prequels, sequels and spinoffs, Jackman has starred in such films as The Prestige (2006) and The Greatest Showman (2017), and garnered an Oscar nomination for his contributions to the big screen adaptation of Les Misérables (2012). The Australian actor has also earned acclaim for his stage work, winning a Tony Award in 2004 for his performance in The Boy from Oz.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxOTA3NTgxNDUz/hugh-jackman-16599916-1-402.jpg"
+    },
+    {
+        name: "Daniel Craig",
+        dob: "10/12/1968",
+        bio: "Born in 1968 in Chester, England, Daniel Craig moved to London when he was 16 to join the National Youth Theatre. Following his big screen debut in The Power of One, the actor featured in the BAFTA-winning BBC miniseries Our Friends in the North and went on to roles in films like Lara Croft: Tomb Raider and Road to Perdition. Craig also worked with director Steven Spielberg on Munich and in 2006 was seen as James Bond in Casino Royale, reinvigorating the franchise. He returned as Bond for the blockbusters Quantum of Solace, Skyfall and Spectre, while starring in other major features like The Girl with the Dragon Tattoo and Logan Lucky.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwMjg4MDE4OTU4/daniel-craig-201264-2-402.jpg"
+    },
+    {
+        name: "Viola Davis",
+        dob: "8/11/1965",
+        bio: "Viola Davis grew up in Rhode Island, where she began acting — first in high school, and then at Rhode Island College. After attending the Juilliard School of Performing Arts, Davis made her Broadway debut in 1996 in Seven Guitars. She has won Tony Awards for her performances in King Hedley II (2001) and a revival of August Wilson's Fences (2010), which co-starred Denzel Washington. Her film work includes Doubt (2008), for which she received an Oscar nomination, The Help (2011), Ender's Game (2013) and Get on Up (2014). In 2015 she became the first African American woman to win an Emmy for Outstanding Lead Actress in a Drama Series for her work on the television series How to Get Away with Murder. She reprised her role playing Rose Maxson in the 2016 film adaptation of Fences, directed by and co-starring Washington, for which she received an Oscar for Best Supporting Actress in 2017. ",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU1Mjg4OTA3Mjc5/viola-davis-20724203-1-402.jpg"
+    },
+    {
+        name: "Halle Berry",
+        dob: "8/14/1966",
+        bio: "Halle Berry is an acclaimed actress and former beauty queen. For her performance in Monster's Ball in 2001, she became the first African American woman to win an Academy Award for best actress. She has also enjoyed prominent roles in Jungle Fever (1991), X-Men (2000) and its sequels, Swordfish (2001), Die Another Day (2002), Gothika (2003), Cloud Atlas (2012) and Kidnap (2017).",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxODYxNjQwNzE3/halle-berry-9542339-1-402.jpg"
+    },
+    {
+        name: "Tom Hanks",
+        dob: "7/9/1956",
+        bio: "Tom Hanks began performing with the Great Lakes Shakespeare Festival in 1977, later moving to New York City. He starred in the television sitcom Bosom Buddies, but became far more known when he starred in the Ron Howard film Splash. He went on to headline many more popular and acclaimed movies, including Big, Forrest Gump and Cast Away, en route to becoming one of the most powerful and well-respected actors in Hollywood.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxNjUxNTk3ODM3/tom-hanks-9327661-1-402.jpg"
+    },
+    {
+        name: "Johnny Depp",
+        dob: "7/9/1963",
+        bio: "Johnny Depp landed his first legitimate movie role in Nightmare on Elm Street (1984). He began studying acting in earnest, the lessons paying off in 1987 when he landed a role on the TV show 21 Jump Street. He has since become known for his willingness to take on darker roles in films such as Edward Scissorhands (1990), Sleepy Hollow (1999) and Alice in Wonderland (2010), as well as his starring efforts in the big-budget Pirates of the Caribbean movies.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTIwNjA4NjM0MDYyNTM4MjUy/johnny-depp-9542522-1-402.jpg"
+    },
+    {
+        name: "Robert Downey Jr.",
+        dob: "4/4/1965",
+        bio: "Born in New York City on April 4, 1965, Robert Downey Jr. began acting as a young child. He made his first film appearances and was a cast member on Saturday Night Live in the 1980s, but his growing success was marred by years of struggles with drug abuse. Eventually turning his life around, he earned a resurgence of critical and popular acclaim, and is considered one of Hollywood's A-list actors.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwNzMzODYwMzY2/robert-downey-jr-9542052-1-402.jpg"
+    },
+    {
+        name: "Leonardo DiCaprio",
+        dob: "11/11/1974",
+        bio: "Leonardo DiCaprio is an actor known for his edgy, unconventional roles. He started out in television before moving on to film, scoring an Oscar nomination for his role in What's Eating Gilbert Grape (1993). In 1997, DiCaprio starred in James Cameron's epic drama Titanic, which made him a star. The actor has also paired up with iconic director Martin Scorsese for several projects, including The Aviator (2004) and The Departed (2006). His more recent films include Inception (2010), Django Unchained (2012), The Wolf of Wall Street (2013) and The Revenant (2015), winning his first Oscar for the latter.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxNjA5MDY0OTcz/leonardo-dicaprio-9273992-1-402.jpg"
+
+    },
+    {
+        name: "Will Smith",
+        dob: "9/25/1968",
+        bio: "After Will Smith met Jeff Townes at age 16, the duo launched a highly successful rap career as DJ Jazzy Jeff & The Fresh Prince. Smith starred on the sitcom The Fresh Prince of Bel-Air for six seasons, before establishing himself as a Hollywood A-lister with Bad Boys (1995) and Independence Day (1996). He has since headlined such popular films as Men in Black (1997) and Hitch (2005) and earned Oscar nominations for Ali (2001) and The Pursuit of Happyness (2006). Smith also earned acclaim for Concussion (2015), before returning to action fare with Suicide Squad (2016).",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwNzQzMTY2NDc4/will-smith-9542165-1-402.jpg"
+    },
+    {
+        name: "Meryl Streep",
+        dob: "6/22/1949",
+        bio: "Meryl Streep began her career on the New York stage in the late 1960s and appeared in several Broadway productions. Streep transitioned to films in the 1970s and soon began earning major accolades, eventually winning Oscars for Kramer vs. Kramer, Sophie's Choice and The Iron Lady, among a league of nominations. Equally able to wow audiences in drama, comedy and musicals, she has come to be considered one of the greatest actresses of our time.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxNzk1NjQ1OTY1/meryl-streep-9497266-1-402.jpg"
+
+    },
+    {
+        name: "Scarlett Johansson",
+        dob: "11/22/1984",
+        bio: "Scarlett Johansson was born in New York City on November 22, 1984. She began acting as a child, and her role in the movie The Horse Whisperer brought her critical acclaim at age 13. Her subsequent successes include Lost in Translation, Girl with a Pearl Earring, The Nanny Diaries, Vicky Cristina Barcelona, Hitchcock and the mega-hit The Avengers. Exploring other artistic avenues, Johansson released her first album in 2008 and the next year she made her Broadway debut. Following her contributions to Ghost in the Shell, Rough Night, Isle of Dogs and Avengers: Endgame, Johansson garnered Oscar nominations for her roles in Marriage Story and Jojo Rabbit.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwNzkxOTI1MjYy/scarlett-johansson-13671719-1-402.jpg"
+    },
+    {
+        name: "Natalie Portman",
+        dob: "6/9/1981",
+        bio: "Born in Israel in 1981, actress Natalie Portman grew up on Long Island, New York, and began modeling at age 11. Her film debut was in The Professional (1994), and she was cast as Queen Amidala in the Star Wars prequels. While continuing her career, Portman earned a degree in psychology from Harvard University. She won the 2010 Best Actress Oscar for her portrayal of a troubled ballerina in Black Swan, before debuting in Marvel's Thor franchise the following year. In 2012, she married dancer Benjamin Millepied, with whom she has two children.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwNzU3MDYwMTEw/natalie-portman-9542326-1-402.jpg"
+    },
+    {
+        name: "Angela Basset",
+        dob: "8/16/1958",
+        bio: "Angela Bassett attended the Yale School of Drama and went on to star in the Tina Turner biopic What's Love Got to Do With It, for which the actress received an Academy Award nomination and a Golden Globe Award. Other films have included Waiting to Exhale, How Stella Got Her Groove Back, Strange Days, Supernova and Mr. 3000. She wed fellow actor Courtney B. Vance in 1997.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU1MjM0NTEyMzk5/angela-bassett-9542651-1-402.jpg"
+    },
+    {
+        name: "Viola Davis",
+        dob: "8/11/1965",
+        bio: "Viola Davis grew up in Rhode Island, where she began acting — first in high school, and then at Rhode Island College. After attending the Juilliard School of Performing Arts, Davis made her Broadway debut in 1996 in Seven Guitars. She has won Tony Awards for her performances in King Hedley II (2001) and a revival of August Wilson's Fences (2010), which co-starred Denzel Washington. Her film work includes Doubt (2008), for which she received an Oscar nomination, The Help (2011), Ender's Game (2013) and Get on Up (2014). In 2015 she became the first African American woman to win an Emmy for Outstanding Lead Actress in a Drama Series for her work on the television series How to Get Away with Murder. She reprised her role playing Rose Maxson in the 2016 film adaptation of Fences, directed by and co-starring Washington, for which she received an Oscar for Best Supporting Actress in 2017. ",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU1Mjg4OTA3Mjc5/viola-davis-20724203-1-402.jpg"
+    },
+    {
+       name: "Morgan Freeman",
+       dob: "6/1/1937",
+       bio: "Morgan Freeman joined the Air Force after high school to become a fighter pilot. He later realized it wasn't what he'd wanted and thus, began his acting career. After years of small parts and limited success, he began to land big roles and win critical and popular acclaim. He's now one of Hollywood's most respected stars." ,
+       img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQyNjgwMDQ0NjE4MTMwNzU4/gettyimages-503700338jpg.jpg"
+    },
+    {
+        name: "Matt Damon",
+        dob: "10/8/1970",
+        bio: "Matt Damon is an American actor whose career took off after starring in and writing 1997's Good Will Hunting with friend Ben Affleck. The duo won an original screenplay Oscar for the project. Damon has since acted in a wide range of movies, including the popular Ocean's Eleven series and the Jason Bourne thrillers. Other major projects on his roster have included The Talented Mr. Ripley, The Adjustment Bureau, Behind the Candelabra and the Golden Globe-winning blockbuster The Martian, directed by Ridley Scott.  ",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwNDkzOTk4NjA2/matt-damon-9265409-1-402.jpg"
+    },
+    {
+        name: "George Clooney",
+        dob: "5/6/1961",
+        bio: "Actor and director George Clooney scored his breakout role in 1992 as Dr. Doug Ross on TV's ER. The hit show led to major film roles, including a turn as Batman. In 2005, Clooney won an Academy Award for his performance in Syriana. TIME magazine has called the actor 'The Last Movie Star,' and People magazine twice named him the 'Sexiest Man Alive.' In 2014, Clooney married international lawyer and activist Amal Alamuddin.",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NTU2MzE2MTg2NzczMDAz/george-clooney-9251385-1-402.jpg"
+    },
+    {
+        name: "Salma Hayek",
+        dob: "9/2/1966",
+        bio: "Salma Hayek got her big break in Hollywood with a role in Robert Rodriguez's Desperado, eventually ascending to the pinnacle of the industry with her Academy Award-nominated turn in Frida. Hayek has also been involved in TV work, notably serving as executive producer of the hit series Ugly Betty. ",
+        img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU1MjQ4Mjc0OTU5/salma-hayek-14514423-1-402.jpg"
     }
+
+
 ]
--- server.js
+++ server.js
@@ -2,10 +2,25 @@ const express = require('express')
 const app = express()
 
 
+app.use(express.static('public'))
+app.use(express.json())
+
+const seed = require('./models/seed.js')
+
+
+
+
+//ROUTES//
 app.get('/' , (req, res) => {
-    res.send('ok')
+    res.render('index.ejs' , {
+        allActors : seed
+    })
+})
+
+app.get('/reference', (req, res) => {
+    res.send(seed)
 })
 
 
-
 app.listen(3000)
+
