ade572eb6e886ce0ef63a14e24b1aea08a4c6dd1
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-03-01 20:07:27 -0500

    finished show

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://localhost:27017/actors', {
     useNewUrlParser: true,
@@ -8,17 +9,16 @@ mongoose.connect('mongodb://localhost:27017/actors', {
 
 const app = express()
 
-
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(express.json())
 
 const seed = require('./models/seed.js')
 const Actor = require('./models/actormodel.js')
 
-
-
-
 //ROUTES//
+
+//INDEX//
 app.get('/' , (req, res) => {
     Actor.find({}, (err, allActors) => {
         res.render('index.ejs', {
@@ -27,11 +27,28 @@ app.get('/' , (req, res) => {
     })
 })
 
+//SHOW//
+app.get('/:id' , (req, res) => {
+    Actor.findById(req.params.id ,(err, foundActor) => {
+        res.render('show.ejs' , {
+            actor : foundActor
+        })
+    })
+})
+
+//NEW//
+app.get('/new' , (req, res) => {
+    res.render('new.ejs')
+})
+
+
+//SEED//
 app.get('/seed', (req, res) => {
     Actor.create(seed, (error, data) => {
         res.redirect('/')
     })
 })
+
 
 
 app.listen(3000)
